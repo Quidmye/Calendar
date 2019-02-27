b112e98a12fb135eda448702051dae124b9cbc66
Denis Koltsov <markmaquire@gmail.com>
2019-02-27 20:34:10 +0200

    2

---
+++ src/migrations/create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->dateTime('start_at');
+            $table->dateTime('end_at');
+            $table->dateTime('reminder_at')->nullable();
+            $table->unsignedInteger('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users')
+              ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}

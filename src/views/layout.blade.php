f571187f5905ee58bc782f133f6161f850730d47
Denis Koltsov <markmaquire@gmail.com>
2019-02-27 20:04:25 +0200

    1

--- src/views/layout.blade.php
+++ src/views/layout.blade.php
@@ -72,7 +72,7 @@
                         <img src="/assets/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                       </div>
                       <h4>
-                        Support Team
+                        Support Team 
                         <small><i class="fa fa-clock-o"></i> 5 mins</small>
                       </h4>
                       <p>Why not buy a new awesome theme?</p>

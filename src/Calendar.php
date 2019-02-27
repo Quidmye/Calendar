083f7ef739079e68f08f905b51383778a7d2d75d
Quidmye <48060919+Quidmye@users.noreply.github.com>
2019-02-27 18:39:49 +0200

    Update Calendar.php

--- src/Calendar.php
+++ src/Calendar.php
@@ -6,8 +6,6 @@ class Calendar
 {
     public static function load()
     {
-        $this->loadViewsFrom(__DIR__.'/views', 'Quidmye');
-        
         return view('Qcalendar::layout');
     }
    

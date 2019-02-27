b21960a17b32dfe0df80895edce9427622e5d9b5
Quidmye <48060919+Quidmye@users.noreply.github.com>
2019-02-27 17:12:46 +0200

    Create PackageServiceProvider.php

---
+++ src/Providers/PackageServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Calendar\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    
+    
+    public function register()
+    {
+        //
+    }
+    
+    
+    public function boot()
+    {
+        //
+    }
+}

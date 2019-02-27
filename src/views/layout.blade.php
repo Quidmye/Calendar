5a21458467f849f9fb45a1d09bb47bb9948b4c30
Denis Koltsov <markmaquire@gmail.com>
2019-02-27 19:39:50 +0200

    assets

--- src/views/layout.blade.php
+++ src/views/layout.blade.php
@@ -801,22 +801,12 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 3 -->
-<script src="../bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="../bower_components/jquery-ui/jquery-ui.min.js"></script>
-<!-- Slimscroll -->
-<script src="../bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="../bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
+<script src="/assets/Quidmye/js/jquery/dist/jquery.min.js"></script>
+<script src="/assets/Quidmye/js/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="/assets/Quidmye/js/jquery-ui/jquery-ui.min.js"></script>
 <script src="/assets/Quidmye/js/adminlte.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../dist/js/demo.js"></script>
-<!-- fullCalendar -->
-<script src="../bower_components/moment/moment.js"></script>
-<script src="../bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
+<script src="/assets/Quidmye/js/moment/moment.js"></script>
+<script src="/assets/Quidmye/js/fullcalendar/dist/fullcalendar.min.js"></script>
 <!-- Page specific script -->
 <script>
   $(function () {

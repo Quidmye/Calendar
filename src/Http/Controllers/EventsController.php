bed8c6ba19066b889109c297a6c3441e0e15b5c6
Denis Koltsov <markmaquire@gmail.com>
2019-02-27 21:18:25 +0200

    1

---
+++ src/Http/Controllers/EventsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Quidmye\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class EventsController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function add()
+    {
+        return view('Qcalendar::add_event');
+    }
+}

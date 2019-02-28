03c40b71ffbb732b71f5bedf28af3a0b38bbf705
Denis Koltsov <markmaquire@gmail.com>
2019-02-28 04:17:11 +0200

    5

--- src/Http/Requests/EventAddRequest.php
+++ src/Http/Requests/EventAddRequest.php
@@ -2,6 +2,7 @@
 
 namespace Quidmye\Http\Requests;
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Http\FormRequest;
 
 class EventAddRequest extends FormRequest
@@ -21,15 +22,19 @@ class EventAddRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(Request $request)
     {
-        return [
+        $rules = [
             "name" => "required|max:155|min:5",
             "start_time" => "required|date_format:d.m.Y H:i",
             "end_time" => "required|date_format:d.m.Y H:i|after_or_equal:start_time",
-            "reminder_time" => "required_if:remember,|date_format:d.m.Y H:i|before:start_time",
             "description" => "max:500",
             "event_files" =>  "nullable|mimetypes:audio/mpeg,audio/ogg,audio/webm,image/gifimage/jpeg,image/pjpeg,image/png"
         ];
+
+        if($request->has('reminder')){
+          $rules['reminder_time'] = "required_if:remember,|date_format:d.m.Y H:i|before:start_time";
+        }
+        return $rules;
     }
 }

aa2c8e29ff59eaa065a99714a5949681dd57be6d
Denis Koltsov <markmaquire@gmail.com>
2019-02-28 10:57:08 +0200

    s

--- src/views/events/show.blade.php
+++ src/views/events/show.blade.php
@@ -7,7 +7,7 @@
           <div class="box box-primary">
             <div class="box-header">
               <div class="box-title"><h3 class="box-title"><strong>{{ $event->name }}</strong></h3>  {{ $event->start_at }} @if($event->start_at != $event->end_at) - {{ $event->end_at }} @endif</div>
-              <div class="box-tools pull-right"><a class='btn btn-warning btn-sm' href="/">Изменить</a> <a href="/" class='btn btn-danger btn-sm'>Удалить</a></div>
+              <div class="box-tools pull-right"><a class='btn btn-warning btn-sm' href="{{ route('events.edit', $event) }}">Изменить</a> <a href="{{ route('events.delete', $event) }}" class='btn btn-danger btn-sm'>Удалить</a></div>
             </div>
             <div class="box-body pad table-responsive">
               @if($event->description)

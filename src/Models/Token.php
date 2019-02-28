6eeb8f5fe290377bf9d98d2adb52790edaf07c09
Denis Koltsov <markmaquire@gmail.com>
2019-02-28 15:34:01 +0200

    ddsadas

--- src/Models/Token.php
+++ src/Models/Token.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Token extends Model
 {
     protected $table = 'users_push_tokens';
-    
+
+    protected $fillable = [
+        'token', 'user_id'
+    ];
 }

74d777e64ba10fad4a46151ca817bb5352095555
Denis Koltsov <markmaquire@gmail.com>
2019-02-28 08:55:35 +0200

    s

--- src/Http/Controllers/EventsController.php
+++ src/Http/Controllers/EventsController.php
@@ -45,7 +45,7 @@ class EventsController extends Controller
         $event = Event::create($data);
 
         if($request->has('event_files')){
-          $this->uploadFile($request->event_files);
+          $this->uploadFile($request->event_files, $event);
         }
 
         if($request->ajax()){
@@ -57,7 +57,7 @@ class EventsController extends Controller
         }
     }
 
-    private function uploadFile($data){
+    private function uploadFile($data, $event){
       $dir = public_path('storage/' . date('Y/m/d'));
       if(!is_dir($dir)){
         mkdir($dir ,0777, true);

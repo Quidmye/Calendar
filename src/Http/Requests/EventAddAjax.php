4cf4c68fe6d322c558315bd6e0d16c1774a15ce0
Denis Koltsov <markmaquire@gmail.com>
2019-02-28 14:17:52 +0200

    dsadsa

--- src/Http/Requests/EventAddAjax.php
+++ src/Http/Requests/EventAddAjax.php
@@ -5,7 +5,7 @@ namespace Quidmye\Http\Requests;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Http\FormRequest;
 
-class EventAddRequest extends FormRequest
+class EventAddAjaxRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.

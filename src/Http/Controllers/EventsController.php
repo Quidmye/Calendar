98b25c6f5ed5f50cb7065a41317b5600803e90f6
Denis Koltsov <markmaquire@gmail.com>
2019-02-28 06:17:57 +0200

    Models :D

--- src/Http/Controllers/EventsController.php
+++ src/Http/Controllers/EventsController.php
@@ -4,6 +4,7 @@ namespace Quidmye\Http\Controllers;
 
 use Illuminate\Routing\Controller;
 use Quidmye\Http\Requests\EventAddRequest;
+use Quidmye\Models\Event;
 
 class EventsController extends Controller
 {
@@ -25,6 +26,12 @@ class EventsController extends Controller
 
     public function add_post(EventAddRequest $request)
     {
-
+        $event = Event::create([
+          'name'        =>  $request->input('name'),
+          'start_at'    =>  $request->input('start_time'),
+          'end_at'      =>  $request->input('end_time'),
+          'description' =>  $request->input('description')
+        ]);
+        $event['reminder_at'] = $request->has('reminder') ? $request->input('reminder_time') : null;
     }
 }
---
+++ src/Models/Event.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Quidmye\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    protected $table = 'events';
+
+    public $timestamps = false;
+}

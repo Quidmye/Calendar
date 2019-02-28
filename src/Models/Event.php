ca04fd465e6a6e9276ff36cd544a0eda77c844a7
Denis Koltsov <markmaquire@gmail.com>
2019-02-28 06:52:46 +0200

    s

--- src/Models/Event.php
+++ src/Models/Event.php
@@ -3,6 +3,7 @@
 namespace Quidmye\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Event extends Model
 {
@@ -20,14 +21,14 @@ class Event extends Model
     }
 
     public function setStartAtAttribute($value){
-      $this->attributes['start_at'] = \Carbon::parse($value)->format("Y-m-d H:i:00");
+      $this->attributes['start_at'] = Carbon::parse($value)->format("Y-m-d H:i:00");
     }
 
     public function setEndAtAttribute($value){
-      $this->attributes['end_at'] = \Carbon::parse($value)->format("Y-m-d H:i:00");
+      $this->attributes['end_at'] = Carbon::parse($value)->format("Y-m-d H:i:00");
     }
 
     public function setReminderAtAttribute($value){
-      $this->attributes['reminder_at'] = \Carbon::parse($value)->format("Y-m-d H:i:00");
+      $this->attributes['reminder_at'] = Carbon::parse($value)->format("Y-m-d H:i:00");
     }
 }

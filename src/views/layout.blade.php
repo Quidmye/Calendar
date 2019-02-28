2026ac98b8f8789aa351b19c9bae08460fcb378d
Denis Koltsov <markmaquire@gmail.com>
2019-02-28 04:58:20 +0200

    s

--- src/views/layout.blade.php
+++ src/views/layout.blade.php
@@ -781,7 +781,7 @@
         $(this).ajaxSubmit({  // pre-submit callback
             success:       showResponse,  // post-submit callback
             error:         function(xhr, textStatus, errorThrown)  {
-              data = JSON.parse((xhr.responseText);
+              data = JSON.parse(xhr.responseText);
               formELement.prepend("<div class=\"callout callout-danger\">" + data.join("<br />") + "</div>");
             }
         });

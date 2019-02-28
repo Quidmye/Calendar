42bf7fd1e5ac911c989987ff5b1129f53653720c
Denis Koltsov <markmaquire@gmail.com>
2019-02-28 06:19:17 +0200

    s

--- src/Http/Controllers/EventsController.php
+++ src/Http/Controllers/EventsController.php
@@ -26,12 +26,16 @@ class EventsController extends Controller
 
     public function add_post(EventAddRequest $request)
     {
-        $event = Event::create([
+        $data = [
           'name'        =>  $request->input('name'),
           'start_at'    =>  $request->input('start_time'),
           'end_at'      =>  $request->input('end_time'),
           'description' =>  $request->input('description')
-        ]);
-        $event['reminder_at'] = $request->has('reminder') ? $request->input('reminder_time') : null;
+        ];
+        $data['reminder_at'] = $request->has('reminder') ? $request->input('reminder_time') : null;
+
+        $event = Event::create($data);
+
+        return $event;
     }
 }

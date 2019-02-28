82432f903452472372dcffaeed398d1ff59a945f
Denis Koltsov <markmaquire@gmail.com>
2019-02-28 11:58:08 +0200

    dasd

--- src/Http/Requests/EventEditRequest.php
+++ src/Http/Requests/EventEditRequest.php
@@ -5,7 +5,7 @@ namespace Quidmye\Http\Requests;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Http\FormRequest;
 
-class EventAddRequest extends FormRequest
+class EventEditRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.

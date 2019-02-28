25b0daf54532ff2ec0ad5840c6b1cb1485fc625b
Denis Koltsov <markmaquire@gmail.com>
2019-02-28 04:07:36 +0200

    s

--- src/Http/Requests/EventAddRequest.php
+++ src/Http/Requests/EventAddRequest.php
@@ -27,7 +27,7 @@ class EventAddRequest extends FormRequest
             "name" => "required|max:155|min:5",
             "start_time" => "required|date_format:d.m.Y H:i",
             "end_time" => "required|date_format:d.m.Y H:i|after_or_equal:start_time",
-            "reminder_time" => "required_with:remember|date_format:d.m.Y H:i|before:start_time",
+            "reminder_time" => "required_if:remember,|date_format:d.m.Y H:i|before:start_time",
             "description" => "max:500",
             "event_files" =>  "nullable|mimetypes:audio/mpeg,audio/ogg,audio/webm,image/gifimage/jpeg,image/pjpeg,image/png"
         ];

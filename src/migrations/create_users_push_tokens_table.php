048415051274b7d855970ef342a5d86be2caf339
Denis Koltsov <markmaquire@gmail.com>
2019-02-28 15:09:42 +0200

    fds

--- src/migrations/create_users_push_tokens_table.php
+++ src/migrations/create_users_push_tokens_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersPushTokensTable extends Migration
+class UsersPushTokensTable extends Migration
 {
     /**
      * Run the migrations.

8c71716ce37e001ffdcf3597d2f13d3d7a8286b0
Denis Koltsov <markmaquire@gmail.com>
2019-02-28 12:58:53 +0200

    dsadas

--- src/views/events/list.blade.php
+++ src/views/events/list.blade.php
@@ -114,7 +114,7 @@
           data.start = new Date(data.start);
           data.end = new Date(data.end);
         });
-        console.log('data');
+        console.log(data);
         return data;
         }
     });

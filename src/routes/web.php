81284bcc772308ee26293cc7850d568503e12a46
Denis Koltsov <markmaquire@gmail.com>
2019-03-03 00:41:50 +0200

    dsa

--- src/routes/web.php
+++ src/routes/web.php
@@ -25,33 +25,4 @@ Route::prefix('event')->namespace('Quidmye\Http\Controllers')->group(function ()
 
 Route::prefix('events')->namespace('Quidmye\Http\Controllers')->group(function () {
     Route::get('/', 'EventsController@list')->name('events.list');
-    Route::get('/ee', function(){
-      $content = array(
-        "en" => 'Testing Message'
-        );
-
-    $fields = array(
-        'app_id' => "9f1ab67d-067f-4d5a-9491-7368d9d293de",
-        'included_segments' => array('All'),
-        'data' => array("foo" => "bar"),
-        'contents' => $content
-    );
-
-    $fields = json_encode($fields);
-print("\nJSON sent:\n");
-print($fields);
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
-                                               'Authorization: Basic 9f1ab67d-067f-4d5a-9491-7368d9d293de'));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_HEADER, FALSE);
-    curl_setopt($ch, CURLOPT_POST, TRUE);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-
-    $response = curl_exec($ch);
-    curl_close($ch);
-    });
 });

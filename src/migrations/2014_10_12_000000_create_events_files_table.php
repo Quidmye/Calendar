7eab7bc7439bfc7c5cc9814b13f0e6fbc36c11e5
Denis Koltsov <markmaquire@gmail.com>
2019-03-03 01:01:10 +0200

    dsadsa

--- src/migrations/2014_10_12_000000_create_events_files_table.php
+++ src/migrations/2014_10_12_000000_create_events_files_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class EventsFilesTable extends Migration
+class CreateEventsFilesTable extends Migration
 {
     /**
      * Run the migrations.

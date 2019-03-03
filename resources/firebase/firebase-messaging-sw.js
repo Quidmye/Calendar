cacaf2e3602965a3244b4048f8bd43e3edfdb81d
Denis Koltsov <markmaquire@gmail.com>
2019-03-03 03:22:33 +0200

    dsa

--- resources/firebase/firebase-messaging-sw.js
+++ resources/firebase/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.8.5/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.8.5/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
